ac201cb10f4680bb99ad7723b6e53dcda6c991fe
Jayden Whiley <jaydenwhiley@gmail.com>
2023-11-14 19:14:08 +1000

    fix for ultrawide monitor

--- src/Components/DashContainer.tsx
+++ src/Components/DashContainer.tsx
@@ -1,7 +1,7 @@
 export default ({ children }: React.PropsWithChildren<{}>) => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="lg:w-3/4 lg:h-3/4 w-full lg:h-full p-2 flex flex-col justify-center">{children}</div>
+      <div className="lg:w-3/4 lg:h-3/4 w-full max-w-[2000px] lg:h-full p-2 flex flex-col justify-center">{children}</div>
     </div>
   );
 };
